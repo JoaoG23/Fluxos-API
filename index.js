a13d401ebcd641c1bd019aa743614d13a85a920c
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-12-17 10:26:54 -0300

    Mudando Coisa

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./server');
-const port = process.env.PORT_SERVIDOR || 3000;
-
-app.listen(port ,() =>{
-    console.log("Servidor Rodando em PORTA ", port);
-});
--- server.js
+++ server.js
@@ -1,20 +1,7 @@
+require('dotenv').config();
+const app = require('./app');
+const port = process.env.PORT_SERVIDOR || 3000;
 
-// --- -initialize --------
-
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-
-const controleDeRouters = require('./src/Routers/ControleDeRouters');
-app.use(cors());
-app.use("/v1" ,express.json(), controleDeRouters);
-
-// Para monolito front end
-// app.use(express.static(path.join(__dirname, 'view')));
-// +app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'view', 'index.html'));
-// });
-
-module.exports = app;
+app.listen(port ,() =>{
+    console.log("Servidor Rodando em PORTA ", port);
+});
